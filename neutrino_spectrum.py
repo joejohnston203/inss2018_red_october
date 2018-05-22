cb1b92029bf5dcd3bf8c0e99442c6a3531c0d40f
bbullard <bbullard@g.harvard.edu>
2018-05-22 16:25:40 +0200

    Added code to notebook

--- neutrino_spectrum.py
+++ neutrino_spectrum.py
@@ -8,14 +8,14 @@ from array import array
 import matplotlib.pyplot as plt
 
 xmin = 1800
-xmax = 10000
+xmax = 20000
 energies = np.linspace(xmin, xmax)
 u235_spectrum = ReactorTools.dRdEnu_U235(energies)
-fig = plt.figure()
-plt.semilogy(energies,u235_spectrum,'r--',label='U-235',linewidth=2)
+fig = plt.figure(figsize=(10,8))
+plt.semilogy(energies/1000,u235_spectrum,'r--',label='U-235',linewidth=2)
 plt.legend(prop={'size':11})
-plt.xlabel('Neutrino Energy (keV)', fontsize=14)
-plt.ylabel('Spectrum (nu/keV/fission)', fontsize=14)
-plt.savefig("reactor_spectrum_u235.png")
+plt.xlabel('Neutrino Energy (MeV)', fontsize=14)
+plt.ylabel('Flux Spectrum (nu/keV/fission)', fontsize=14)
+#plt.savefig("reactor_spectrum_u235.png")
 plt.show()
 
